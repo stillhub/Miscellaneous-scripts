2afd2e34f0a751c8c8c07b0a61a10c07662fa6bd
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2022-12-04 21:28:01 +0000

    Script to get battery health report from remote PC

---
+++ BatteryReport/BatteryReport.ps1
@@ -0,0 +1,28 @@
+﻿# Script to get battery health report of remote machine. Saves report on a network drive.
+# Script created by Jared Stillwell
+# Version: 1.0
+#
+# Notes:
+# 
+
+
+([switch]$Elevated)
+function CheckAdmin{
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+if ((CheckAdmin) -eq $false) {
+    if ($elevated) {
+        # could not elevate, quit
+    }
+    else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -ExecutionPolicy Bypass -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition)) | Out-Null
+    }
+    Exit
+}
+
+CheckAdmin
+$computer = Read-Host "Enter machine hostname"
+$s = New-PSSession -ComputerName $computer #-Credential Domain01\User01
+Invoke-Command -Session $s -ScriptBlock {powercfg /batteryreport /output \\NETWORKCOMPUTERNAME\C$\Temp\$computer.html}
+pause
