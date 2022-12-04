3987743c656d23122fd78a39525a619eefeabccc
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2022-12-04 18:44:49 +0000

    Adding Old file remover script

---
+++ RemoveOldFiles/OldFIleRemover.ps1
@@ -0,0 +1,22 @@
+﻿# Script to delete old files. Useful for removing old downloads on RDS servers
+# Script created by Jared Stillwell
+# Version: 1.0
+
+$rootdir = @('\\TARGETNETWORKFOLDER1\','\\TARGETNETWORKFOLDER2\','\\TARGETNETWORKFOLDER3\')
+$Daysback = "-15"
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+
+
+Foreach($root in $rootdir){
+    cd $root
+    $Paths = (Get-ChildItem).Name
+    foreach($Path in $Paths){
+        $FullDir = $Path
+        #$FullDir = $Path + "\Downloads"
+        #Get-ChildItem $FullDir | Where-Object {$_.LastWriteTime -lt $DatetoDelete}
+        Get-ChildItem $FullDir | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
+
+    }
+}
+
