294600a1df13c4e8f806107e725b39cfb293257a
Jared Stillwell <75730966+stillhub@users.noreply.github.com>
2023-01-24 14:22:27 +0000

    Script to bulk change folder permissions

---
+++ BulkChangePermissions.ps1
@@ -0,0 +1,17 @@
+﻿#$folders = dir
+$folders = Import-Csv 1.csv
+$folderpath = $folders.Name
+
+foreach($DirPath in $folderpath){
+    write-host "Removing security from $DirPath"
+    icacls $DirPath /remove:g "GROUP 1" /t /c
+    icacls $DirPath /remove:g "GROUP 2" /t /c
+    #icacls $DirPath /deny $DirPath:W /c
+    #$permission = $DirPath + ":(M)"
+    #icacls $DirPath /grant $permission /t /c
+}
+
+dir | export-csv -path usersDesktop.csv -NoTypeInformation
+
+cd D:\DocumentFolder
+cd D:\DesktopFolder
